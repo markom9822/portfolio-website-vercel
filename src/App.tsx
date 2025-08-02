a2f541f4432ef711e441f5b2199fb3dfe0928548
Mark O Meara <markomeara98@gmail.com>
2025-08-02 12:43:36 +0100

    start of work on initial loading screen

--- src/App.tsx
+++ src/App.tsx
@@ -55,109 +55,117 @@ export function App() {
     }
   }
 
+  const renderDesktop = () => {
 
-  return (
-    <div className='flex min-h-screen text-zinc-300 bg-black'>
-      <aside className="w-80 h-screen bg-zinc-900/60 border-zinc-700 backdrop-blur-sm p-6 flex flex-col justify-between shadow-xl sticky top-0">
+    return (
+      <div className='flex min-h-screen text-zinc-300 bg-black'>
+        <aside className="w-80 h-screen bg-zinc-900/60 border-zinc-700 backdrop-blur-sm p-6 flex flex-col justify-between shadow-xl sticky top-0">
 
-        <div className='flex flex-col items-center space-y-6'>
+          <div className='flex flex-col items-center space-y-6'>
 
-          <motion.img
-            src={profilePic}
-            loading='eager'
-            fetchPriority='high'
-            draggable={false}
-            className='rounded-full w-32 h-32 object-cover shadow'
-            initial={{ scale: 0.8, opacity: 0 }}
-            animate={{ scale: 1, opacity: 1 }}
-            transition={{ duration: 0.6 }}
-          />
+            <motion.img
+              src={profilePic}
+              loading='eager'
+              fetchPriority='high'
+              draggable={false}
+              className='rounded-full w-32 h-32 object-cover shadow'
+              initial={{ scale: 0.8, opacity: 0 }}
+              animate={{ scale: 1, opacity: 1 }}
+              transition={{ duration: 0.6 }}
+            />
+
+            <motion.div
+              className='text-center space-y-3'
+              initial={{ opacity: 0, y: -40 }}
+              animate={{ y: 0, opacity: 1, transition: { type: 'spring', stiffness: 200, damping: 25 } }}
+            >
+              <h2 className='text-2xl text-zinc-300 font-bold font-text'>
+                Mark O Meara
+              </h2>
+              <p className='text-base text-zinc-400 font-text'>
+                [ Software Engineer ]
+              </p>
+            </motion.div>
+
+            <nav className='flex flex-col w-full space-y-2 relative z-0'>
+              {tabs.map(({ label, Icon }) => (
+
+                <div
+                  key={label}
+                  className='relative z-0'>
+
+                  {activeTab === label && (
+                    <motion.div
+                      layoutId="highlight"
+                      className="absolute inset-0 bg-white/10 rounded z-[-1]"
+                      transition={{ type: "spring", stiffness: 500, damping: 40 }}
+                    />
+                  )}
+                  <button
+                    onClick={() => setActiveTab(label)}
+                    className={`flex text-base items-center space-x-5 px-4 py-2 w-full rounded duration-200 cursor-pointer ${activeTab === label
+                      ? "text-white"
+                      : "hover:bg-zinc-800 text-zinc-500/80"
+                      }`}
+                  >
+                    {Icon && <Icon size={20} />}
+                    <span className='font-text'>{label}</span>
+                  </button>
+                </div>
+              ))}
+            </nav>
+          </div>
 
           <motion.div
-            className='text-center space-y-3'
-            initial={{ opacity: 0, y: -40 }}
-            animate={{ y: 0, opacity: 1, transition: { type: 'spring', stiffness: 200, damping: 25 } }}
-          >
-            <h2 className='text-2xl text-zinc-300 font-bold font-text'>
-              Mark O Meara
-            </h2>
-            <p className='text-base text-zinc-400 font-text'>
-              [ Software Engineer ]
-            </p>
+            className='flex justify-center space-x-5 pt-6 border-t border-zinc-700 text-zinc-400'
+            initial="hidden"
+            animate="visible">
+
+            <motion.a
+              href="https://github.com/markom9822"
+              target="_blank"
+              rel="noreferrer"
+              className="transition-colors hover:text-white"
+              aria-label="Github"
+              initial={{ opacity: 0, y: 30 }}
+              animate={{ y: 0, opacity: 1, transition: { type: 'spring', stiffness: 200, damping: 30 } }}
+            >
+              {githubIcon}
+            </motion.a>
+
+            <motion.a
+              href="https://www.linkedin.com/in/marko-meara/"
+              target="_blank"
+              rel="noreferrer"
+              className="transition-colors hover:text-white"
+              aria-label="LinkedIn"
+              initial={{ opacity: 0, y: 30 }}
+              animate={{ y: 0, opacity: 1, transition: { type: 'spring', stiffness: 200, damping: 30 } }}
+            >
+              {linkedinIcon}
+            </motion.a>
           </motion.div>
+        </aside>
 
-          <nav className='flex flex-col w-full space-y-2 relative z-0'>
-            {tabs.map(({ label, Icon }) => (
-
-              <div
-                key={label}
-                className='relative z-0'>
-
-                {activeTab === label && (
-                  <motion.div
-                    layoutId="highlight"
-                    className="absolute inset-0 bg-white/10 rounded z-[-1]"
-                    transition={{ type: "spring", stiffness: 500, damping: 40 }}
-                  />
-                )}
-                <button
-                  onClick={() => setActiveTab(label)}
-                  className={`flex text-base items-center space-x-5 px-4 py-2 w-full rounded duration-200 cursor-pointer ${activeTab === label
-                    ? "text-white"
-                    : "hover:bg-zinc-800 text-zinc-500/80"
-                    }`}
-                >
-                  {Icon && <Icon size={20} />}
-                  <span className='font-text'>{label}</span>
-                </button>
-              </div>
-            ))}
-          </nav>
-        </div>
-
-        <motion.div
-          className='flex justify-center space-x-5 pt-6 border-t border-zinc-700 text-zinc-400'
-          initial="hidden"
-          animate="visible">
-
-          <motion.a
-            href="https://github.com/markom9822"
-            target="_blank"
-            rel="noreferrer"
-            className="transition-colors hover:text-white"
-            aria-label="Github"
-            initial={{ opacity: 0, y: 30 }}
-            animate={{ y: 0, opacity: 1, transition: { type: 'spring', stiffness: 200, damping: 30 } }}
-          >
-            {githubIcon}
-          </motion.a>
-
-          <motion.a
-            href="https://www.linkedin.com/in/marko-meara/"
-            target="_blank"
-            rel="noreferrer"
-            className="transition-colors hover:text-white"
-            aria-label="LinkedIn"
-            initial={{ opacity: 0, y: 30 }}
-            animate={{ y: 0, opacity: 1, transition: { type: 'spring', stiffness: 200, damping: 30 } }}
-          >
-            {linkedinIcon}
-          </motion.a>
-        </motion.div>
-      </aside>
-
-      <main className="flex-1 h-screen overflow-y-auto p-6 md:p-10 
+        <main className="flex-1 h-screen w-full overflow-y-auto p-6 md:p-10 
         [&::-webkit-scrollbar]:[width:10px]
       [&::-webkit-scrollbar-thumb]:bg-zinc-600
         [&::-webkit-scrollbar-thumb]:rounded-xs
         [&::-webkit-scrollbar-corner]:bg-zinc-900
         overflow-scroll">
-        {handleContentsSection(activeTab)}
-      </main>
+          {handleContentsSection(activeTab)}
+        </main>
 
-    </div>
+      </div>
+    )
+  }
 
+  return (
+    <>
+    {renderDesktop()}
+    </>
   )
+  
 }
 
 export default App
