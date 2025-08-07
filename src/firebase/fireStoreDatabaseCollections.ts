be4faa4f21a9c4ac2181f626bd41e8b9ff9a021e
Mark O Meara <markomeara98@gmail.com>
2025-08-07 10:48:17 +0100

    added in firestore collection names

---
+++ src/firebase/fireStoreDatabaseCollections.ts
@@ -0,0 +1,10 @@
+
+
+export const fireStoreCollections = (
+    {
+        aboutMeSection: "aboutMe",
+        projectsSection: "projects",
+        postsSection: "posts",
+        educationSection: "education",
+    }
+)
