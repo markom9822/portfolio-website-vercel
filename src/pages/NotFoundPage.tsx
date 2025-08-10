b6f57d60a93fdbaab80d20d199a45001d193a547
Mark O Meara <markomeara98@gmail.com>
2025-08-10 16:42:35 +0100

    more clean up

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -4,7 +4,7 @@ export const NotFoundPage = () => {
     return (
         <div className="min-h-screen bg-[#0f0f0f] text-white p-6 font-text">
             <div className="max-w-5xl mx-auto space-y-12">
-                <div className="flex justify-between items-center">
+                <div className="flex justify-center items-center">
                     <h1 className="text-4xl font-bold font-text tracking-tight">
                         Not Found
                     </h1>
